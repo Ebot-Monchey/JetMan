314a3c83bf2ccd58aaecfdf404bd1cae43ee9865
Vuyolwethu <vngqaqhu@gmail.com>
2015-05-17 09:48:59 +0200

    Declared the Panel class widget.

--- JetMan/graphics.h
+++ JetMan/graphics.h
@@ -3,6 +3,7 @@
 #ifndef GRAPHICS_H
 #define GRAPHICS_H
 
+#include <vector>
 #include "utils.h"
 
 namespace JetMan {
@@ -45,6 +46,35 @@ namespace JetMan {
 			 * Called when the mouse clicks the widget.
 			 */
 			virtual void onClick(JetMan::Utils::Rectangle mouse) = 0;
+		};
+
+		/*
+		 * Widget that can contain other widgets.
+		 */
+		class Panel : public Widget {
+		public:
+			/*
+			 * Creates a new panel.
+			 */
+			Panel();
+			/*
+			 * Adds the widget to this panel.
+			 */
+			void addWidget(Widget *widget);
+			/*
+			 * Draws the panel by drawing all its children widgets.
+			 */
+			void draw();
+			/*
+			 * Called when the mouse is hovering over the widget.
+			 */
+			void onMouseOver(JetMan::Utils::Rectangle mouse);
+			/*
+			 * Called when the mouse clicks the widget.
+			 */
+			void onClick(JetMan::Utils::Rectangle mouse);
+		private:
+			std::vector<Widget*> widgets;			// The widgets on the panel.
 		};
 	}
 }
